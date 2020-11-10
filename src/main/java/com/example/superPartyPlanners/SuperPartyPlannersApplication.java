ecaebb5d89740cbbddeefc36dbc563d199861478
David <416dfranco@gmail.com>
2020-11-10 17:17:41 -0500

    first commit

---
+++ src/main/java/com/example/superPartyPlanners/SuperPartyPlannersApplication.java
@@ -0,0 +1,13 @@
+package com.example.superPartyPlanners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperPartyPlannersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperPartyPlannersApplication.class, args);
+	}
+
+}
